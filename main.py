f1ec5301192e04eb60b3bcedf2a4addd746ecd14
Luna <102811601+Luna5829@users.noreply.github.com>
2024-05-15 23:52:57 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,48 @@
+import os
+
+import requests
+
+imagePath = r"test_image"
+channelId = 0
+
+headers = {"Authorization": "auth token goes here obviously"}
+
+payload = {
+    "files": [
+        {
+            "filename": "image.png",
+            "file_size": os.path.getsize(imagePath),
+        }
+    ]
+}
+
+req = requests.post(
+    f"https://discord.com/api/v9/channels/{channelId}/attachments",
+    json=payload,
+    headers=headers,
+).json()
+
+imageAttachment = req["attachments"][0]
+uploadUrl = imageAttachment["upload_url"]
+uploadFilename = imageAttachment["upload_filename"]
+
+requests.put(uploadUrl, data=open(imagePath, "rb"))
+
+payload = {
+    "attachments": [
+        {
+            "id": "0",
+            "filename": "image.png",
+            "uploaded_filename": uploadFilename,
+            "is_remix": True,
+        }
+    ]
+}
+
+req = requests.post(
+    f"https://discord.com/api/v9/channels/{channelId}/messages",
+    json=payload,
+    headers=headers,
+).text
+
+print(req)
